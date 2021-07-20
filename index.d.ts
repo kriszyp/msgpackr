5ddd74ebf6860aa8fcbcc5d3eaf6c15a3c862555
Kris Zyp <kriszyp@gmail.com>
2021-07-20 07:55:29 -0600

    Fix stream typing, #31

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,9 @@
 export { Unpackr, Decoder, unpack, decode, addExtension, FLOAT32_OPTIONS, clearSource } from './unpack'
 export { Packr, Encoder, pack, encode } from './pack'
+import { Transform } from 'stream'
 
 export as namespace msgpackr;
-export class UnpackrStream {
+export class UnpackrStream extends Transform {
 }
-export class PackrStream {
-	write(value: any): void
-	end(value?: any): void
+export class PackrStream extends Transform {
 }
