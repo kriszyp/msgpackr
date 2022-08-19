531a6f56a54add716006938f78c68861c8c2b38b
Kris Zyp <kriszyp@gmail.com>
2022-08-18 22:14:00 -0600

    Single source type definition and copy and reference it, #75

--- pack.d.ts
+++ pack.d.ts
@@ -1,9 +1,1 @@
-import { Unpackr } from './unpack.js'
-export { addExtension, FLOAT32_OPTIONS } from './unpack.js'
-export class Packr extends Unpackr {
-	pack(value: any): Buffer
-	encode(value: any): Buffer
-}
-export class Encoder extends Packr {}
-export function pack(value: any): Buffer
-export function encode(value: any): Buffer
+export { Unpackr, Decoder, Packr, Encoder, pack, encode, unpack, decode, addExtension, FLOAT32_OPTIONS } from '.'
